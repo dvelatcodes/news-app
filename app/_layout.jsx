2a402a5b8f1181eb6133f0951f4e418df4cc0b18
love olubummo <olubummolove3@gmail.com>
2024-06-25 04:01:17 +0100

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { Tabs } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Tabs/>
+  )
+}
+
+export default RootLayout
---
+++ app/news/categories/countryCategories/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CountryCategories = () => {
+  return (
+    <View>
+      <Text>
+        CountryCategories
+      </Text>
+    </View>
+  )
+}
+
+export default CountryCategories
+
+const styles = StyleSheet.create({})
